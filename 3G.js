30aaf445448760f1d92af9ef8824e4976733effd
HoucemEddine <h_hamidi@esi.dz>
2013-12-17 12:17:30 +0100

    Data Base of the 3G Offres

---
+++ 3G.js
@@ -0,0 +1,12 @@
+{Offres :[
+{id:1  , nom:"Ooredoo750"       , operateur:"Nejma"   , type:1 ,   navigation:750 , abonnement:1000    , Credit:0},
+{id:2  , nom:"Ooredoo100"       , operateur:"Nejma"  , type:2 ,  navigation:50  , abonnement:100    , Credit:0},
+{id:3  , nom:"Ooredoo3G"       , operateur:"Nejma"  , type:1 ,  navigation:3072  , abonnement:2500    , Credit:0},
+{id:4  , nom:"Pass1Jour"       , operateur:"Mobilis"  , type:2 ,  navigation:50  , abonnement:100    , Credit:0},
+{id:5  , nom:"Pass7Jours"       , operateur:"Mobilis"  , type:3 ,  navigation:250  , abonnement:500    , Credit:0},
+{id:6  , nom:"Pass30Jours"       , operateur:"Mobilis"  , type:1 ,  navigation:1024  , abonnement:1500    , Credit:0},
+{id:7  , nom:"DarynetBronze"       , operateur:"Mobilis"  , type:4 ,  navigation:500  , abonnement:750    , Credit:"100 SMS Gratuits"},
+{id:8  , nom:"DarynetSilver"       , operateur:"Mobilis"  , type:4 ,  navigation:1024  , abonnement:1000    , Credit:"100 SMS Gratuits"},
+{id:9  , nom:"DarynetGold"       , operateur:"Mobilis"  , type:4 ,  navigation:4096  , abonnement:3000    , Credit:"100 SMS Gratuits"}
+]}
+
